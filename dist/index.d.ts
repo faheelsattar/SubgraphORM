caecc53ef68f92000bad83e9ae822ad0a78ea64d
faheelsattar <faheelsattar@yahoo.com>
2021-10-05 09:18:10 +0500

    removing dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const checker: () => void;
-export default checker;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-
-var checker = function () {
-    console.log("i am working 2");
-};
-
-exports["default"] = checker;
-//# sourceMappingURL=index.js.map
