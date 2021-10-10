e2051e88e7be171f28c73270ec3cdeae41b6aba0
faheelsattar <faheelsattar@yahoo.com>
2021-10-11 00:14:17 +0500

    updated file name <SubgraphORM, Filter>

--- src/SubgraphORM.ts
+++ src/SubgraphORM.ts
@@ -3,12 +3,14 @@ class SubgraphORM {
   private query: String;
   private entity: Array<String>;
   private factory: Factory;
-  
+
   constructor(entity: Array<String>) {
     this.entity = entity;
     this.factory = new Factory(this.entity);
     this.factory.createSkeleton();
   }
+
+  
 
 }
 
